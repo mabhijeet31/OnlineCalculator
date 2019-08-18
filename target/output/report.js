21f37262f98be1ed0a7edddbae94331f632bf2db
mabhijeet31 <45145752+mabhijeet31@users.noreply.github.com>
2019-08-18 21:23:49 +0530

    Adding report folder

---
+++ target/output/report.js
@@ -0,0 +1,445 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Verify_CE_Function.feature");
+formatter.feature({
+  "line": 1,
+  "name": "verify Clear function of the calculator",
+  "description": "",
+  "id": "verify-clear-function-of-the-calculator",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "001 - Test to verify clear function",
+  "description": "",
+  "id": "verify-clear-function-of-the-calculator;001---test-to-verify-clear-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I validate the clear button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 28563913957,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_clear_button()"
+});
+formatter.result({
+  "duration": 11886643,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 4320853820,
+  "status": "passed"
+});
+formatter.uri("Verify_Minus_Function.feature");
+formatter.feature({
+  "line": 1,
+  "name": "verify minus function of the calculator",
+  "description": "",
+  "id": "verify-minus-function-of-the-calculator",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "001 - Test to verify minus function",
+  "description": "",
+  "id": "verify-minus-function-of-the-calculator;001---test-to-verify-minus-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I substract \"9\" with \"5\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I validate the result \"4\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 16564797491,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "9",
+      "offset": 13
+    },
+    {
+      "val": "5",
+      "offset": 22
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_substract_with(String,String)"
+});
+formatter.result({
+  "duration": 6341246501,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "4",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 3168760817,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 4736100711,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "002 - Test to verify minus function",
+  "description": "",
+  "id": "verify-minus-function-of-the-calculator;002---test-to-verify-minus-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "I substract \"2\" with \"8\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "I validate the result \"-6\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 14368680076,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 13
+    },
+    {
+      "val": "8",
+      "offset": 22
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_substract_with(String,String)"
+});
+formatter.result({
+  "duration": 4581324179,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "-6",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 3142769291,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3412587311,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "003 - Test to verify minus function",
+  "description": "",
+  "id": "verify-minus-function-of-the-calculator;003---test-to-verify-minus-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 20,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 21,
+  "name": "I substract \"0\" with \"5\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 22,
+  "name": "I validate the result \"-5\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 12611421611,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "0",
+      "offset": 13
+    },
+    {
+      "val": "5",
+      "offset": 22
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_substract_with(String,String)"
+});
+formatter.result({
+  "duration": 3858719801,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "-5",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 3261321391,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3561886450,
+  "status": "passed"
+});
+formatter.uri("Verify_division_Function.feature");
+formatter.feature({
+  "line": 1,
+  "name": "verify division function of the calculator",
+  "description": "",
+  "id": "verify-division-function-of-the-calculator",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "001 - Test to verify division function",
+  "description": "",
+  "id": "verify-division-function-of-the-calculator;001---test-to-verify-division-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I division \"9\" with \"3\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I validate the result \"3\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 16758643636,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "9",
+      "offset": 12
+    },
+    {
+      "val": "3",
+      "offset": 21
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_division_with(String,String)"
+});
+formatter.result({
+  "duration": 4135448709,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "3",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 3014600535,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3384456659,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "002 - Test to verify division function",
+  "description": "",
+  "id": "verify-division-function-of-the-calculator;002---test-to-verify-division-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "I division \"8\" with \"5\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "I validate the result \"1.6\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 13237652881,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "8",
+      "offset": 12
+    },
+    {
+      "val": "5",
+      "offset": 21
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_division_with(String,String)"
+});
+formatter.result({
+  "duration": 3376574568,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1.6",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 5148526,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3852553030,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "003 - Test to verify division function",
+  "description": "",
+  "id": "verify-division-function-of-the-calculator;003---test-to-verify-division-function",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 20,
+  "name": "I navigate to calcultor page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 21,
+  "name": "I division \"0\" with \"4\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 22,
+  "name": "I validate the result \"4\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_navigate_to_calcultor_page()"
+});
+formatter.result({
+  "duration": 13248093198,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "0",
+      "offset": 12
+    },
+    {
+      "val": "4",
+      "offset": 21
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_division_with(String,String)"
+});
+formatter.result({
+  "duration": 3734569282,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "4",
+      "offset": 23
+    }
+  ],
+  "location": "Minus_DivisionFunctionalityStepDefinition.i_validate_the_result(String)"
+});
+formatter.result({
+  "duration": 3147505131,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3183358786,
+  "status": "passed"
+});
+});
